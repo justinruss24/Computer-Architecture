e01ae86e51b0a264ca28be0ba8a33c41e63ffccd
Justin Russell <Justin@justins-macbook.lan>
2020-09-14 15:02:37 -0700

    day 1 mvp met

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -32,6 +32,12 @@ class CPU:
         for instruction in program:
             self.ram[address] = instruction
             address += 1
+    
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+    
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = [MDR]
 
 
     def alu(self, op, reg_a, reg_b):
@@ -65,4 +71,27 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+
+        while self.running:
+            ir = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if ir == HLT:
+                # exit
+                self.running = False
+            elif ir == LDI:
+                # set the value of a register to an integer
+                self.reg[operand_a] = [operand_b]
+                self.pc += 3
+            elif ir == PRN:
+                # print numeric value stored in the given register
+                print(self.reg[operand_a])
+                self.pc += 2
+            else:
+                print(f'unknown instruction {ir} at address {self.pc}')
+                self.running = False
+                
